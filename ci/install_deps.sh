1010a7079405594dca4e0fb0fa976e651b51a3e2
Joseph Ballschneider <jballschneider@yext.com>
2020-06-10 09:57:19 -0400

    consulting prep

--- ci/install_deps.sh
+++ ci/install_deps.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+# JAMBO
 npm install -g serve npx
 npm install
+
+# CONSULTING
+# If you wish to add consulting pages to this repo, uncomment the following lines
+# to ensure YextCI handles those pages appropriately. 
+# You should also add your consulting pages package.json file to the 
+# ci_config.dependencies.required_files array as well as output the 
+# ci_config.output_directories to include compiled templates.
+# cd src
+# npm install
--- ci/serve.sh
+++ ci/serve.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
+# JAMBO
 serve -l 8080 desktop/ &
 grunt watch
+
+# CONSULTING
+# If you wish to add consulting pages to this repo, use the following commands
+# instead of the JAMBO commands.
+# grunt watch &
+# cd src
+# pager -port=8080 -pagesdir=/repo -templatedir=src/node_modules/answers-experience-builder/src/templates:src/node_modules/components-common:src/node_modules/components-util:src/templates -staticdirs=src/.tmp &
+# grunt compile:dev watch
