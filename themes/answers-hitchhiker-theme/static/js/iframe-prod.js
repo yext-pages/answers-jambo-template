2cf72b75cfa92bcf6f67f29ac4161fc2c4f834de
Connor Anderson <canderson@yext.com>
2021-04-27 11:48:42 -0400

    update jambo and theme

--- themes/answers-hitchhiker-theme/static/js/iframe-prod.js
+++
@@ -1,5 +0,0 @@
-import { generateIFrame } from './iframe-common';
-import InjectedData from './models/InjectedData';
-
-const prodDomain = new InjectedData().getProdDomain()
-generateIFrame(prodDomain);
