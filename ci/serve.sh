78faf4c66ba3a5eac38a946029bc8ac4e41cc2e0
Tom Meyer <tmeyer@yext.com>
2022-12-05 16:26:13 -0500

    Update serve.sh

--- ci/serve.sh
+++ ci/serve.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 export NODE_OPTIONS="--max-old-space-size=1024"
 
-#This will use development mode while in the Yext code editor. Set to "false" to test the production build.
-export IS_DEVELOPMENT_PREVIEW='true'
-
 serve -l 8080 desktop/ &
 grunt watch
